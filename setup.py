b336fba9c32325c67c95d6dad25869d5e7857623
Ajay Rangarajan <armandyam@gmail.com>
2025-07-13 08:31:02 +0200

    Update setup.py
    Updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='travel-score',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         'geopy>=2.3.0',
